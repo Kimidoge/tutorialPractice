6ae22d06aaf1cceddb4634d842d38b5b05ac8019
Kimidoge <eilhamapek@gmail.com>
2020-12-22 08:19:04 +0800

    Tutorial 2 Recycler View Intent and getIntent WebView
    Tutorial 3
    Share intent
    
    Tutorial 4
    Volley

--- app/src/main/java/com/example/tutorialpractice/DetailsActivity.java
+++ app/src/main/java/com/example/tutorialpractice/DetailsActivity.java
@@ -1,12 +1,19 @@
 package com.example.tutorialpractice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    private String url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +22,33 @@ public class DetailsActivity extends AppCompatActivity {
 
         String name = getIntent().getStringExtra("name");  //catch here
 
-        String url = "https://en.m.wikipedia.org/wiki/Android_"+ name.replace(" ", "_");
 
+        url = "https://en.m.wikipedia.org/wiki/Android_"+ name.replace(" ", "_");
+        Toast.makeText(this, url, Toast.LENGTH_SHORT).show();
         WebView webView = findViewById(R.id.webView);
 
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl(url);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.action_details, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch(item.getItemId()) {
+            case R.id.action_share:
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, url);
+                startActivity(intent);
+                break;                  //will create a chooser
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/tutorialpractice/MainActivity.java
+++ app/src/main/java/com/example/tutorialpractice/MainActivity.java
@@ -6,18 +6,33 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.util.LruCache;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.NetworkImageView;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
-    Version[] versions = {
+   /* Version[] versions = {
             new Version("Cupcake", "API 3", R.drawable.cupcake),
             new Version("Donut", "API 4", R.drawable.donut),
             new Version("Eclair", "API 5, 6, 7", R.drawable.eclair),
@@ -33,7 +48,10 @@ public class MainActivity extends AppCompatActivity {
             new Version("Oreo", "API 26, 27", R.drawable.oreo)
 
 
-    };
+    }; */
+
+    ArrayList<Version> versions = new ArrayList<Version>();
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,25 +62,69 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         MyAdapter myAdapter = new MyAdapter();
-        myAdapter.addElements(versions);
+        //myAdapter.addElements(versions);
         recyclerView.setAdapter(myAdapter);
 
+        queue = Volley.newRequestQueue(this);
+
+        JsonArrayRequest request =
+                new JsonArrayRequest(Request.Method.GET, " https://raw.githubusercontent.com/kenobicjj/android/main/tutorial4.json ",
+        null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        versions.clear();
+                        for(int i = 0;i < response.length();i++) {
+                            try {   //do try catch incase it doesnt work
+                                JSONObject item = response.getJSONObject(i);
+                                String name = item.getString("name");
+                                String description = item.getString("description");
+                                String icon = item.getString("icon");
+                                Version version =new Version(name, description, icon);
+                                versions.add(version);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        myAdapter.addElements(versions);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                    }});
+            queue.add(request);
     }
     class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         ArrayList<Version> elements = new ArrayList<Version>();
         @NonNull
         @Override
-        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {       //create viewholder by creating interface
             View rowView = getLayoutInflater().inflate(R.layout.row, parent, false);  // will return a row
             return new MyViewHolder(rowView);
         }
 
         @Override
-        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {   //get the widgets from interface and bind it to memebr elements
 
             holder.textView.setText(elements.get(position).getName());   // called from the Version
             holder.textView2.setText(elements.get(position).getDescription());
-            holder.imageView.setImageResource(elements.get(position).getIcon());
+            //holder.imageView.setImageResource(elements.get(position).getIcon());
+
+            String iconUrl =
+                    "https://raw.githubusercontent.com/kenobicjj/android/main/"+elements.get(position).getIcon();
+            final LruCache<String, Bitmap> cache =new LruCache<String, Bitmap>(20);
+            holder.imageView.setImageUrl(iconUrl, new ImageLoader(queue, new
+                    ImageLoader.ImageCache() {
+                        @Override
+                        public Bitmap getBitmap(String url) {
+                            return cache.get(url);
+                        }
+                        @Override
+                        public void putBitmap(String url, Bitmap bitmap) {
+                            cache.put(url, bitmap);
+                        }
+                    }));
 
 
 
@@ -71,18 +133,18 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public int getItemCount() {
             return elements.size();  //should return 13 as got 13 array
-        }
+        }   //returns the number of items
 
-        public void addElements(Version[] versions) {   // put in Version arrays, then add all, and notify
+        public void addElements(ArrayList<Version>versions) {   // put in Version arrays, then add all, and notify
             elements.clear();
-            elements.addAll(Arrays.asList(versions));
+            elements.addAll(versions);
             notifyDataSetChanged();
         }
 
-        class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {  //binds widget to textview
             public TextView textView;
             public TextView textView2;
-            public ImageView imageView;
+            public NetworkImageView imageView;
 
             public MyViewHolder(@NonNull View itemView) {
                 super(itemView);
@@ -97,10 +159,10 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) {   //what happens when clicked
 
                 String name = elements.get(getAdapterPosition()).getName();
-                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);   //launch new activity main->details
                 intent.putExtra("name", name);
                 startActivity(intent);
 
